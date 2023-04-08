422401bc873b01829cc1e17f49e7b7c2382fa3cb
Kacperski2115 <kacperski2512@gmail.com>
2023-04-08 22:58:00 +0200

    Dodanie niezbędnych bibliotek i deklaracja głównych zmiennych

--- app/src/main/java/com/example/checkiprojekt/MainActivity.kt
+++ app/src/main/java/com/example/checkiprojekt/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.checkiprojekt
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioGroup
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var PrzedmiotyKoszyk: Map<String, List<String>>
+    private var WybraneArtykuły: MutableList<String> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        PrzedmiotyKoszyk = mapOf(
+            "Warzywa" to listOf("Pomidor", "Bakłażan", "Ogórek"),
+            "Mięsa" to listOf("Kurczak", "Szynka", "Kiełbasa"),
+            "Pieczywo" to listOf("Bagietka", "Chleb", "Bułka","Bajgiel")
+        )
     }
 }
